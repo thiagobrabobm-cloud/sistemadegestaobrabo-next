305b902b67c64f80ea4347dbad8388e85035de03
thiagobrabobm-cloud <thiagobrabobm@gmail.com>
2025-08-21 22:41:23 -0300

    chore(auth): wrap app with SessionProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
-import { SessionProvider } from "next-auth/react"
-import '../styles/globals.css' // Mantenha os estilos globais, se houver
+import { SessionProvider } from "next-auth/react";
+import "../styles/globals.css"; // mantenha se existir
 
-// O Componente e as pageProps são recebidos aqui
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    // O SessionProvider agora envolve todo o site
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
-  )
+  );
 }
